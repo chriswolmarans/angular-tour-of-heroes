1f556629eb696a72d0e264bca8af09c44e4a02c9
Chris Wolmarans <chris.wolmarans@gmail.com>
2020-09-09 10:46:51 -0700

    Convert styles to tailwind utility styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  @HostBinding('class')
+  get classes(): string {
+    return 'absolute container m-8'; // why absolute??
+  }
 }
+
